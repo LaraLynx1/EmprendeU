9e03537e261c93540057214eac15d6dee807e32e
daniel <duque.murillas@gmail.com>
2025-04-05 15:00:38 -0500

    welcome y navegacion listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,51 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+// import SignUp from './paginas/signup/signup';
+// import SignIn from './paginas/SignIn/SignIn';
+// import './App.css';
+
+// function App() {
+//   const [currentView, setCurrentView] = useState('signin'); // 'signin' o 'signup'
+
+//   const switchToSignUp = () => {
+//     setCurrentView('signup');
+//   };
+
+//   const switchToSignIn = () => {
+//     setCurrentView('signin');
+//   };
+
+//   return (
+//     <div className='app-container'>
+//       {currentView === 'signin' ? (
+//         <SignIn onSwitchToSignUp={switchToSignUp} />
+//       ) : (
+//         <SignUp onSwitchToSignIn={switchToSignIn} />
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SignUp from './paginas/signup/signup';
 import SignIn from './paginas/SignIn/SignIn';
+import WelcomeScreen from './paginas/welcome/welcome';
 import './App.css';
 
 function App() {
-  const [currentView, setCurrentView] = useState('signin'); // 'signin' o 'signup'
-
-  const switchToSignUp = () => {
-    setCurrentView('signup');
-  };
-
-  const switchToSignIn = () => {
-    setCurrentView('signin');
-  };
-
   return (
-    <div className='app-container'>
-      {currentView === 'signin' ? (
-        <SignIn onSwitchToSignUp={switchToSignUp} />
-      ) : (
-        <SignUp onSwitchToSignIn={switchToSignIn} />
-      )}
-    </div>
+    <Router>
+      <div className='app-container'>
+        <Routes>
+          <Route path="/" element={<Navigate to="/signin" />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/welcome" element={<WelcomeScreen />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
