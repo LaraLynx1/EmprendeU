806c94b4660ef55b8171b56011ed4f2e6d73aad5
LaraLynx1 <lauraivettepenaranda@gmail.com>
2025-05-05 17:43:46 -0500

    inicio firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyA84GCX1DXrWPfsFavzJqTkggG6o8yixJ8',
+	authDomain: 'emprendeu-9f3ce.firebaseapp.com',
+	projectId: 'emprendeu-9f3ce',
+	storageBucket: 'emprendeu-9f3ce.firebasestorage.app',
+	messagingSenderId: '699456315467',
+	appId: '1:699456315467:web:5418a26cff2ab468ba6dce',
+	measurementId: 'G-53RMXTNS41',
+};
+
+const app = initializeApp(firebaseConfig);
