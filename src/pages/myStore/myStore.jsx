e628d01e5a5cf0b8c1f25db2d1f792ecccfea2a6
LaraLynx1 <lauraivettepenaranda@gmail.com>
2025-05-13 23:14:42 -0500

    modal juego listo

--- src/pages/myStore/myStore.jsx
+++ src/pages/myStore/myStore.jsx
@@ -4,6 +4,7 @@ import ProfileBoxC from '../../components/profile-box-C/profile-box-C';
 import ProductCard from '../../components/carta-producto/carta-producto';
 import Navbar from '../../components/navbar/navbar';
 import CreateProductModal from '../../components/CreateProductModal/CreateProductModal';
+import GameProducts from '../../components/GameProducts/GameProducts';
 
 import logo from '../../resources/logo icesi blue.png';
 import arrowback from '../../resources/arrowback.png';
@@ -16,6 +17,7 @@ const MyStore = () => {
 	const { name, id, status, avatar, products } = sellerProfile;
 	const [isEditing, setIsEditing] = useState(false);
 	const [isCreating, setIsCreating] = useState(false);
+	const [isGameList, setisGameList] = useState(false);
 
 	const toggleEditMode = () => {
 		setIsEditing((prev) => !prev);
@@ -40,7 +42,7 @@ const MyStore = () => {
 				<h2 className='store-title'>
 					<em>My store:</em>
 				</h2>
-				<img src={coupon} alt='Game' className='header-icon1' />
+				<img src={coupon} alt='Game' className='header-icon1' onClick={() => setisGameList(true)} />
 				{isEditing ? (
 					<img src={plus} alt='Add Product' className='header-icon' onClick={() => setIsCreating(true)} />
 				) : (
@@ -57,6 +59,7 @@ const MyStore = () => {
 			<Navbar />
 
 			<CreateProductModal isOpen={isCreating} onClose={() => setIsCreating(false)} />
+			<GameProducts isOpen={isGameList} onClose={() => setisGameList(false)} />
 		</div>
 	);
 };
