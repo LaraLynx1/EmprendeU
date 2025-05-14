0e0098e4a67b729450c3dcb35b924932f02b8bea
LaraLynx1 <lauraivettepenaranda@gmail.com>
2025-05-13 21:06:30 -0500

    arreglo css

--- src/components/CreateProductModal/CreateProductModal.jsx
+++ src/components/CreateProductModal/CreateProductModal.jsx
@@ -5,28 +5,25 @@ const CreateProductModal = ({ isOpen, onClose }) => {
 	if (!isOpen) return null;
 
 	return (
-		<div className='modal-overlay'>
-			<div className='modal-content'>
+		<div className='modal-overlayx'>
+			<div className='modal-contentx'>
 				<h2>Create your new product</h2>
-				<div className='image-upload'>Upload image</div>
+				<div className='image-uploadx'>Upload image</div>
 
-				<div className='form-group'>
-					<label>Name:</label>
-					<input type='text' />
+				<div className='form-groupx'>
+					<input type='text' placeholder='Name' />
 				</div>
-				<div className='form-group'>
-					<label>Description:</label>
-					<input type='text' />
+				<div className='form-groupx'>
+					<input type='text' placeholder='Description' />
 				</div>
-				<div className='form-group'>
-					<label>Price:</label>
-					<input type='text' />
+				<div className='form-groupx'>
+					<input type='text' placeholder='Price' />
 				</div>
 
-				<button className='create-btn' onClick={onClose}>
+				<button className='create-btnx' onClick={onClose}>
 					Create!
 				</button>
-				<button className='close-btn' onClick={onClose}>
+				<button className='close-btnx' onClick={onClose}>
 					✕
 				</button>
 			</div>
