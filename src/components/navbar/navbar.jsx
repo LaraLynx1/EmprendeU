f04499f7e6dd10aacbc100c002dbe631fd9eab6b
Geraldine Sastoque <gerisastoque3009@gmail.com>
2025-06-08 22:16:56 -0500

    Arreglo codigo en funcionalidades

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,43 +6,43 @@ import userIcon from '../../resources/user.png';
 import './navbar.css';
 
 const Navbar = () => {
-	const navigate = useNavigate();
-	const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-	const getButtonContent = (path, icon, altText, displayText) => {
-		const isActive = location.pathname === path;
-		return (
-			<>
-				<img src={icon} alt={altText} className={isActive ? 'active-icon' : ''} />
-				{isActive && <span className='page-name'>{displayText}</span>}
-			</>
-		);
-	};
+  const getButtonContent = (path, icon, altText, displayText) => {
+    const isActive = location.pathname === path;
+    return (
+      <>
+        {!isActive && <img src={icon} alt={altText} className="nav-icon" />}
+        {isActive && <span className='page-name'>{displayText}</span>}
+      </>
+    );
+  };
 
-	return (
-		<div className='navbar'>
-			<button
-				className={`nav-btn ${location.pathname === '/dashboard' ? 'active' : ''}`}
-				onClick={() => navigate('/dashboard')}
-			>
-				{getButtonContent('/dashboard', homeIcon, 'Home', 'Inicio')}
-			</button>
+  return (
+    <div className='navbar'>
+      <button
+        className={`nav-btn ${location.pathname === '/dashboard' ? 'active' : ''}`}
+        onClick={() => navigate('/dashboard')}
+      >
+        {getButtonContent('/dashboard', homeIcon, 'Home', 'Inicio')}
+      </button>
 
-			<button
-				className={`nav-btn ${location.pathname === '/favorites' ? 'active' : ''}`}
-				onClick={() => navigate('/favorites')}
-			>
-				{getButtonContent('/favorites', starIcon, 'Star', 'Favoritos')}
-			</button>
+      <button
+        className={`nav-btn ${location.pathname === '/favorites' ? 'active' : ''}`}
+        onClick={() => navigate('/favorites')}
+      >
+        {getButtonContent('/favorites', starIcon, 'Star', 'Favoritos')}
+      </button>
 
-			<button
-				className={`nav-btn ${location.pathname === '/perfil-personal' ? 'active' : ''}`}
-				onClick={() => navigate('/perfil-personal')}
-			>
-				{getButtonContent('/perfil-personal', userIcon, 'User', 'Perfil')}
-			</button>
-		</div>
-	);
+      <button
+        className={`nav-btn ${location.pathname === '/perfil-personal' ? 'active' : ''}`}
+        onClick={() => navigate('/perfil-personal')}
+      >
+        {getButtonContent('/perfil-personal', userIcon, 'User', 'Perfil')}
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
